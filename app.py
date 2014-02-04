b88d8ff39d2021f99ce73372356cca5766f05632
Dean Hall <dwhall256@gmail.com>
2014-02-03 22:33:21 -0600

    app: Putting script code into functions.  Making loop global explicit.

--- app.py
+++ app.py
@@ -5,13 +5,20 @@ import asyncio, signal
 
 
 def on_sigint():
+    global loop
     print("Bye.")
     loop.stop()
 
 
-loop = asyncio.get_event_loop()
-loop.add_signal_handler(signal.SIGINT, on_sigint)
-loop.add_signal_handler(signal.SIGTERM, on_sigint)
+def main():
+    global loop
+    loop = asyncio.get_event_loop()
+    loop.add_signal_handler(signal.SIGINT, on_sigint)
+    loop.add_signal_handler(signal.SIGTERM, on_sigint)
 
-print("Event loop running forever, press CTRL+c to quit.")
-loop.run_forever()
+    print("Event loop running forever, press CTRL+c to quit.")
+    loop.run_forever()
+
+
+if __name__ == "__main__":
+    main()
