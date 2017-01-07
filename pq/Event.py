d22cf8dd5fd7f68e838c559b8f42b049b53f674e
Dean Hall <dwhall256@gmail.com>
2017-01-07 17:28:07 -0600

    Added Event module that has been in my personal copy but not in git
    Okay, now things should work for someone else trying out this repo

---
+++ pq/Event.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+
+from .Signal import Signal
+
+
+Event = namedtuple("Event", ["signal", "value"])
+
+# Instantiate the reserved (system) events
+Event.EMPTY = Event(Signal.EMPTY, None)
+Event.ENTRY = Event(Signal.ENTRY, None)
+Event.EXIT = Event(Signal.EXIT, None)
+Event.INIT = Event(Signal.INIT, None)
+
+# The order of this tuple MUST match their respective signals
+Event.reserved = (Event.EMPTY, Event.ENTRY, Event.EXIT, Event.INIT)
