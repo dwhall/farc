8e81435fed586958e5914d4d06c3bc314eeea8c8
Dean Hall <dwhall256@gmail.com>
2017-01-07 17:13:41 -0600

    Rename file to match class name

--- pq/__init__.py
+++ pq/__init__.py
@@ -1,6 +1,6 @@
 from .Signal import Signal
 from .Event import Event
-from .EventProcessor import TimeEvent
+from .TimeEvent import TimeEvent
 from .Hsm import Hsm
 from .Framework import Framework
 from .Ahsm import Ahsm
