394499371618e35f1ed60b2f28b89a9b18686064
Dean Hall <dwhall256@gmail.com>
2017-02-05 23:47:45 -0600

    Added an example that demonstrates a fast and slow timer.

---
+++ examples/oneonethousand.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+
+import asyncio
+
+from pq import *
+
+
+class Mississippi(Ahsm):
+
+    @staticmethod
+    def initial(me, event):
+        print("initial")
+    
+        me.te1000 = TimeEvent("TE1000")
+        me.te1000.postEvery(me, 0.001)
+
+        me.te1 = TimeEvent("TE1")
+        me.te1.postEvery(me, 1)
+
+        me._count = 0
+
+        return me.tran(me, Mississippi.count)
+
+
+    @staticmethod
+    def count(me, event):
+        sig = event.signal
+        if sig == Signal.ENTRY:
+            print("count enter")
+            return me.handled(me, event)
+
+        elif sig == Signal.TE1:
+            print(me._count, "millis")
+            return me.handled(me, event)
+
+        elif sig == Signal.TE1000:
+            me._count += 1
+            return me.handled(me, event)
+
+        return me.super(me, me.top)
+
+
+if __name__ == "__main__":
+    print("Check to see how much CPU% a simple 1ms periodic function uses.")
+    ms = Mississippi(Mississippi.initial)
+    ms.start(0)
+
+    loop = asyncio.get_event_loop()
+    loop.run_forever()
+    loop.close()
